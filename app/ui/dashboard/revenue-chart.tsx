0f47e859b09690d7c92e162f5228e6cb6d17e8ca
Jainam <91124384+jainam03@users.noreply.github.com>
2025-06-09 13:51:35 +0530

    completed chapter 9

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,6 +2,8 @@ import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitions';
+import { fetchRevenue } from '@/app/lib/data';
+import './revenue-chart.css';
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,11 +11,8 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart(){
+  const revenue = await fetchRevenue()
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
@@ -33,8 +32,8 @@ export default async function RevenueChart({
       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
           <div
-            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-            style={{ height: `${chartHeight}px` }}
+            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex revenue-y-axis revenue-y-axis--custom-height"
+            // style={{ '--chart-height': `${chartHeight}px` } as React.CSSProperties}
           >
             {yAxisLabels.map((label) => (
               <p key={label}>{label}</p>
@@ -44,10 +43,8 @@ export default async function RevenueChart({
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
-                className="w-full rounded-md bg-blue-300"
-                style={{
-                  height: `${(chartHeight / topLabel) * month.revenue}px`,
-                }}
+                className={`w-full rounded-md bg-blue-300 revenue-bar`}
+                data-bar-height={(chartHeight / topLabel) * month.revenue}
               ></div>
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
                 {month.month}
